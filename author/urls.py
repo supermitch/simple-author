459d2ae52f15d3e97dadb88844c50e606f6090c1
Mitch LeBlanc <supermitch@gmail.com>
2014-11-17 22:57:46 -0800

    Add books to admin and URLs

--- author/urls.py
+++ author/urls.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'author.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^books/', include('books.urls')),
 )
--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from books.models import Book, Chapter
 # Register your models here.
+
+admin.site.register(Book)
+admin.site.register(Chapter)
+
