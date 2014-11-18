e223d5c7c2d2ceae1d9a13a010b49114dd856f81
Mitch LeBlanc <supermitch@gmail.com>
2014-11-17 23:04:40 -0800

    Add extended User profile

--- books/admin.py
+++ books/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from books.models import Book, Chapter
-# Register your models here.
+from books.models import Author
+from books.models import Book
+from books.models import Chapter
 
+admin.site.register(Author)
 admin.site.register(Book)
 admin.site.register(Chapter)
 
--- books/models.py
+++ books/models.py
@@ -1,11 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    """ Extend the User model w/ Author information. """
+    user = models.OneToOneField(User)
+    website = models.CharField(max_length=100)
+    bio = models.CharField(max_length=500)
+
 class Book(models.Model):
+    """ A book contains everything. """
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    description = models.CharField(max_length=200)
 
 class Chapter(models.Model):
+    """ A chapter is the fundamental container of content. """
     book = models.ForeignKey(Book)
     order = models.IntegerField(default=0)
     title = models.CharField(max_length=200)
