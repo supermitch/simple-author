d8656c278d8ed2933831ba06bfda630047bd80ca
Mitch LeBlanc <supermitch@gmail.com>
2014-11-23 23:06:58 -0800

    Redirect after submit works

--- books/urls.py
+++ books/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, include, url
 
-from books.views import IndexView, NewBookView
+import books.views
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', IndexView.as_view(), name='home'),
-    url(r'^new/$', NewBookView.as_view(), name='new_book'),
-    url(r'^edit/(?P<book>[0-9]+)/$$', EditBookView.as_view(), name='edit_book'),
+    url(r'^$', books.views.IndexView.as_view(), name='home'),
+    url(r'^new/$', books.views.NewBookView.as_view(), name='new_book'),
+    url(r'^edit/(?P<book_pk>[0-9]+)/$$', books.views.EditBookView.as_view(), name='edit_book'),
 )
 
--- books/views.py
+++ books/views.py
@@ -22,8 +22,9 @@ class NewBookView(View):
     def post(self, request):
         form = NewBookForm(request.POST)
         if form.is_valid():
+            # TODO: Create book record in DB
             print('Successfully created a book.')
-            redirect('edit_book', book=1)
+            return redirect('edit_book', book_pk=1)
         else:
             context = {'form': form}
             return render(request, 'books/new_book.html', context)
@@ -34,7 +35,11 @@ class NewBookView(View):
 
 class EditBookView(View):
     """ Modify a book's settings and layout, but not content. """
-    def get(self, request):
-        context = {}
+    def get(self, request, book_pk):
+        context = {'book_pk': book_pk}
         return render(request, 'books/edit_book.html', context)
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(EditBookView, self).dispatch(*args, **kwargs)
 
