a7cdf7770fb0d6984d9cc799fdea6fad02bb60ad
Mitch LeBlanc <supermitch@gmail.com>
2014-11-24 19:17:29 -0800

    Add sections to books.models

--- books/models.py
+++ books/models.py
@@ -9,13 +9,49 @@ class Author(models.Model):
 
 class Book(models.Model):
     """ A book contains everything. """
+    author = models.ForeignKey(User)
     title = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateField('date published', blank=True)
     description = models.CharField(max_length=200, blank=True)
+
+FRONT_MATTER_CHOICES = [
+   #('db_value', 'human readable'),
+    ('half_title', 'Half title'),
+    ('title_page', 'Title Page'),
+    ('colophon', 'Colophon'),
+    ('contents', 'Contents'),
+    ('foreward', 'Foreward'),
+    ('preface', 'Preface'),
+    ('acknowledgment', 'Acknowlegment'),
+    ('introduction', 'Introduction'),
+    ('dedication', 'Dedication'),
+    ('epigraph', 'Epigraph'),
+    ('prologue', 'Prologue'),
+]
+
+BACK_MATTER_CHOICES = [
+    ('epilogue', 'Epilogue'),
+    ('afterward', 'Afterward'),
+    ('conclusion', 'Conclusion'),
+    ('postscript', 'Postscript'),
+    ('appendix', 'Appendix'),
+    ('glossary', 'Glossary'),
+    ('bibliography', 'Bibliography'),
+    ('index', 'Index'),
+    ('colophon', 'Colophon'),
+]
+
+SECTION_CHOICES = FRONT_MATTER_CHOICES + BACK_MATTER_CHOICES
+
+class Section(models.Model):
+    book = models.ForeignKey(Book)
+    section_type = models.CharField(max_length=15, choices=SECTION_CHOICES)
+    content = models.TextField(blank=True, default='')
 
 class Chapter(models.Model):
     """ A chapter is the fundamental container of content. """
     book = models.ForeignKey(Book)
     order = models.IntegerField(default=0)
-    title = models.CharField(max_length=200, blank=True)
+    name = models.CharField(max_length=200, blank=True, default='')
+    content = models.TextField(blank=True, default='')
 
