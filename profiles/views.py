095fd3b6d6971c4c969058a6724f74518e13c8a2
Mitch LeBlanc <supermitch@gmail.com>
2014-12-06 21:46:45 -0800

    Fixed profiles view to generate default if needed.

--- profiles/views.py
+++ profiles/views.py
@@ -1,22 +1,34 @@
 from django.shortcuts import render, get_object_or_404
-
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
 from django.utils.decorators import method_decorator
 from django.views.generic import View, TemplateView
 
+import profiles.models
 from profiles.models import Author
 
 class ProfileView(View):
     """ Display a profile defined by the author_name. """
     def get(self, request, display_name=None):
-        if request.user.is_authenticated() and \
-            request.user.author.display_name == display_name:
-                # We're looking at our own profile
-                author = request.user.author
+        print(display_name)
+        if display_name is None:
+            # If we're logged in, get our own profile
+            if request.user.is_authenticated():
+                try:
+                    author = request.user.author
+                except ObjectDoesNotExist:
+                    # Our profile is blank, so let's create one:
+                    author = Author(user=request.user,
+                                    display_name=request.user.username)
+                    author.save()
+            else:
+                raise Http404
         else:
-            # We can only see public profiles
+            # We can only see profiles that are public
+            print('trying..')
             author = get_object_or_404(Author, display_name=display_name,
-                                       privacy='public')
+                                       privacy=profiles.models.PUBLIC)
         context = {
             'author': author,
         }
